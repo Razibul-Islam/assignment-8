d6fee2b87ba329622fb74fe6b5efb139568d549f
Razibul Islam <razibulislam665@gmail.com>
2022-09-29 15:03:48 +0600

    profile section added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import './Cart.css'
+import "./Cart.css";
 
 const Cart = (props) => {
     const { name, img, time, description } = props.gym;
-    return (
-      <div className="cart">
-        <img src={img} alt="" />
-        <h2>{name}</h2>
-        <p className='description'>{description.slice(0, 86)}...</p>
-        <p>
-          Time required: <small>{time}</small>
-        </p>
-        <button className="cart-btn">Add to list</button>
-      </div>
-    );
+  return (
+    <div className="cart">
+      <img src={img} alt="" />
+      <h2>{name}</h2>
+      <p className="description">{description.slice(0, 86)}...</p>
+      <p className="time">
+        Time required: <small>{time}</small>
+      </p>
+      <button className="cart-btn">
+        Add to list
+      </button>
+    </div>
+  );
 };
 
 export default Cart;
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import "./Profile.css";
+import profile from "../../img/profile.jpg";
 
 const Profile = () => {
-    return (
-        <div>
-            <h1>Hello</h1>
+  return (
+    <div className="profile">
+      <div className="personal-info">
+        <img src={profile} alt="" />
+        <div className="info-text">
+          <h3>Razibul Islam</h3>
+          <p>
+            <small>Web developer</small>
+          </p>
         </div>
-    );
+      </div>
+      <div className="break">
+        <h3>Add A Break</h3>
+        <div className="break-time">
+          <p>10s</p>
+          <p>20s</p>
+          <p>30s</p>
+          <p>40s</p>
+          <p>50s</p>
+        </div>
+      </div>
+      <div className="exercise-info">
+        <h3>Exercise Details</h3>
+        <div className="total-exercise">
+          <p>Exercise Details</p>
+          <small>200 seconds</small>
+        </div>
+        <div className="total-exercise">
+          <p>Break time</p>
+          <small>40 seconds</small>
+        </div>
+      </div>
+      <div className="activity-btn">
+        <button>Activity Completed</button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
